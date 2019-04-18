1e2133eaddd657859a88d7e221c9ca2f1a0aebf6
Liyun Li <liyun-li@users.noreply.github.com>
2019-04-18 01:56:09 -0400

    Add more layout.

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,3 +1,4 @@
+import { Grid } from '@material-ui/core';
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Chat from 'src/Chat/Chat';
@@ -13,15 +14,21 @@ class App extends React.Component {
     public render() {
         return (
             <BrowserRouter>
-                <Switch>
-                    <Route path={routes.landing} component={Landing} />
-                    <Route path={routes.chat} component={Chat} />
-                    <Route path={routes.group} component={Group} />
-                    <Route path={routes.profile} component={Profile} />
-                    <Route path={routes.signIn} component={SignIn} />
-                    <Route path={routes.signUp} component={SignUp} />
-                    <Route path={routes.post} component={Post} />
-                </Switch>
+                <Grid container>
+                    <Grid item>
+                    </Grid>
+                    <Grid item>
+                        <Switch>
+                            <Route path={routes.landing} component={Landing} />
+                            <Route path={routes.chat} component={Chat} />
+                            <Route path={routes.group} component={Group} />
+                            <Route path={routes.profile} component={Profile} />
+                            <Route path={routes.signIn} component={SignIn} />
+                            <Route path={routes.signUp} component={SignUp} />
+                            <Route path={routes.post} component={Post} />
+                        </Switch>
+                    </Grid>
+                </Grid>
             </BrowserRouter>
         );
     }
