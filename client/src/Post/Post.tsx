e455b1e5953e1388d3de3843aaf237390ed659d3
Liyun Li <liyun-li@users.noreply.github.com>
2019-04-18 01:34:47 -0400

    Create skeleton.

---
+++ client/src/Post/Post.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class Post extends React.Component {
+    render() {
+        return (
+            <React.Fragment />
+        );
+    }
+}
+
+export default Post;
