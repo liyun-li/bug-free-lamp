12a70b20081aaf2a9c9ebd839c08d4cb52ddbc8c
Liyun Li <liyun-li@users.noreply.github.com>
2019-04-18 01:34:31 -0400

    Reducer template.

---
+++ client/src/reducers/store.ts
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = {
+
+};
+
+const rootReducer = combineReducers(reducers);
+const store = createStore(rootReducer);
+
+export default store;
