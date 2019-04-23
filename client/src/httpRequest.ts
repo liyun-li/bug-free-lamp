7d13d8c506456f5c0353041662c3edd2d20da775
Liyun Li <liyun-li@users.noreply.github.com>
2019-04-23 19:10:46 -0400

    Light abstraction mod

--- client/src/httpRequest.ts
+++ client/src/httpRequest.ts
@@ -3,4 +3,4 @@ import axios from 'axios';
 // bad code
 const BASE_URL = 'http://127.0.0.1:3001';
 
-export const postRequest = (url: string, data: object) => axios.post(BASE_URL + url, data);
+export const postRequest = (url: string, data?: object) => axios.post(BASE_URL + url, data || {});
