d3034011a0b4318ce86f061e267bed782f1318a1
Liyun Li <liyun-li@users.noreply.github.com>
2019-04-24 00:39:12 -0400

    Make list items buttons

--- client/src/Chat/Chat.tsx
+++ client/src/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { createStyles, Divider, Drawer, List, ListItem, Toolbar, withStyles, ListItemText, ListItemAvatar } from '@material-ui/core';
+import { createStyles, Divider, Drawer, List, ListItem, ListItemText, ListItemIcon, Toolbar, withStyles } from '@material-ui/core';
 import MailIcon from '@material-ui/icons/Mail';
 import * as React from 'react';
 
@@ -20,6 +20,7 @@ const styles = createStyles({
         width: '240px'
     },
     contactListHeader: {
+
     }
 });
 
@@ -31,18 +32,18 @@ class Chat extends React.Component<IChatProps> {
             <React.Fragment>
                 <Drawer variant='permanent' className={classes.contactDrawer}>
                     <Toolbar />
-                    <ListItem>
-                        <ListItemAvatar>
-                            <MailIcon />
-                        </ListItemAvatar>
-                        <ListItemText>Inbox</ListItemText>
-                    </ListItem>
-                    <Divider />
                     <List className={classes.contactList}>
+                        <ListItem className={classes.contactListHeader}>
+                            <ListItemIcon>
+                                <MailIcon />
+                            </ListItemIcon>
+                            <ListItemText>Inbox</ListItemText>
+                        </ListItem>
+                        <Divider />
                         {
                             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(e => (
                                 <React.Fragment>
-                                    <ListItem>
+                                    <ListItem button key={e}>
                                         <ListItemText>{`${e} + ${e} = ${e + e}`}</ListItemText>
                                     </ListItem>
                                     <Divider />
