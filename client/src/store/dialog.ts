29c991380765919d5ef5e0602d322ebe686d513c
Liyun Li <liyun-li@users.noreply.github.com>
2019-04-28 05:33:25 -0400

    Fix bug and add an action.

--- client/src/store/dialog.ts
+++ client/src/store/dialog.ts
@@ -4,16 +4,19 @@ import { reducerWithInitialState } from 'typescript-fsa-reducers';
 export interface IDialog {
     signInDisplay: boolean;
     signUpDisplay: boolean;
+    userSearchDisplay: boolean;
 }
 
 const INITIAL_STATE: IDialog = {
     signInDisplay: false,
-    signUpDisplay: false
+    signUpDisplay: false,
+    userSearchDisplay: false
 }
 
-const actionCreator = actionCreatorFactory('Profile');
+const actionCreator = actionCreatorFactory('Dialog');
 export const setSignInDialogDisplay = actionCreator<IDialog['signInDisplay']>('setSignInDialogDisplay');
 export const setSignUpDialogDisplay = actionCreator<IDialog['signUpDisplay']>('setSignUpDialogDisplay');
+export const setUserSearchDialogDisplay = actionCreator<IDialog['userSearchDisplay']>('setUserSearchDialogDisplay');
 
 export const dialogReducer = reducerWithInitialState(INITIAL_STATE)
     .case(setSignInDialogDisplay, (state, payload): IDialog => ({
@@ -23,4 +26,8 @@ export const dialogReducer = reducerWithInitialState(INITIAL_STATE)
     .case(setSignUpDialogDisplay, (state, payload): IDialog => ({
         ...state,
         signUpDisplay: payload
+    }))
+    .case(setUserSearchDialogDisplay, (state, payload): IDialog => ({
+        ...state,
+        userSearchDisplay: payload
     }));
