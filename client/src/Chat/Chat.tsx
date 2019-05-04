05472fd7315d16f08fcce085553e8f77f02039d1
Liyun Li <liyun-li@users.noreply.github.com>
2019-05-04 00:31:42 -0400

    Make chat pretty.

--- client/src/Chat/Chat.tsx
+++ client/src/Chat/Chat.tsx
@@ -1,5 +1,5 @@
-import { Badge, Button, Chip, createStyles, Divider, Drawer, Grid, IconButton, List, ListItem, ListItemIcon, ListItemText, Paper, TextField, Toolbar, withStyles } from '@material-ui/core';
-import { Face, KeyboardReturn, People } from '@material-ui/icons';
+import { Badge, Button, Chip, createStyles, Divider, Drawer, Grid, List, ListItem, ListItemIcon, ListItemText, Paper, TextField, Toolbar, withStyles } from '@material-ui/core';
+import { Face, People } from '@material-ui/icons';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Action, Dispatch } from 'redux';
@@ -30,6 +30,7 @@ interface IChatStyles {
     inputBox: string;
     enterButton: string;
     chatBox: string;
+    message: string;
 }
 
 interface IChatProps extends
@@ -87,7 +88,7 @@ const styles = createStyles({
         flexGrow: 1
     },
     inputBox: {
-        margin: '16px 0 16px 16px',
+        margin: 16,
         padding: 0,
         display: 'flex'
     },
@@ -95,10 +96,12 @@ const styles = createStyles({
         textAlign: 'center'
     },
     chatBox: {
-        left: '3.2%',
+        left: 16,
         position: 'relative',
-        flex: '1 1 auto',
-        display: 'flex'
+        maxWidth: '45%'
+    },
+    message: {
+        marginTop: 8
     }
 });
 // #endregion
@@ -242,101 +245,97 @@ class Chat extends React.Component<IChatProps, IChatState> {
                 <Paper className={classes.messagePanel}>
                     <Grid container direction='row' justify='center' alignItems='flex-end'
                         className={classes.messagePanelGrid}>
-                        <div className={classes.chatBox}>
-                            <Grid item xs={12}>
-                                <Grid container>
-                                        {messages.map(_message => {
-                                            const { username, timestamp, message } = _message;
-                                            return (
-                                                <Grid item xs={12}>
-                                                    <Chip label={`${username} [${timestamp}]: ${message}`}
-                                                        color="primary" icon={<Face />} />
-                                                    <br />
-                                                </Grid>
-                                            )
-                                        })}
+                        <Grid item xs={12}>
+                            <Grid container direction='row' justify='center' alignItems='center'>
+                                <Grid item xs={12}>
+                                    <div className={classes.chatBox}>
+                                        <Grid container>
+                                            {messages.map(_message => {
+                                                const { username, timestamp, message } = _message;
+                                                return (
+                                                    <Grid item xs={12}>
+                                                        <Chip label={`${username} [${timestamp}]: ${message}`}
+                                                            color="primary" icon={<Face />}
+                                                            className={classes.message} />
+                                                        <br />
+                                                    </Grid>
+                                                )
+                                            })}
+                                        </Grid>
+                                    </div>
                                 </Grid>
-                            </Grid>
-                        </div>
 
-                    <Grid item xs={12}>
-                        <Grid container direction='row' justify='center' alignItems='center'>
-                            <Grid item xs={11}>
-                                <TextField className={classes.inputBox}
-                                    placeholder='Hit Enter to send text.'
-                                    variant='outlined' margin='dense'
-                                    onChange={(e) => this.setState({
-                                        ...this.state,
-                                        message: e.target.value
-                                    })} value={message}
-                                    onKeyDown={(e) => {
-                                        if (e.keyCode === keyCodes.enter) {
-                                            // Emit message to server
-                                            postRequest('/chat/send', {
-                                                receiver: currentChat.username,
-                                                message
-                                            });
+                                <Grid item xs={12}>
+                                    <TextField className={classes.inputBox}
+                                        placeholder='Hit Enter to send text.'
+                                        variant='outlined' margin='dense'
+                                        onChange={(e) => this.setState({
+                                            ...this.state,
+                                            message: e.target.value
+                                        })} value={message}
+                                        onKeyDown={(e) => {
+                                            if (e.keyCode === keyCodes.enter) {
+                                                // Emit message to server
+                                                postRequest('/chat/send', {
+                                                    receiver: currentChat.username,
+                                                    message
+                                                });
 
-                                            // Clear message box
-                                            this.setState({
-                                                ...this.state,
-                                                message: ''
-                                            });
-                                        }
-                                    }}
-                                />
-                            </Grid>
-                            <Grid item xs={1} className={classes.enterButton}>
-                                <IconButton>
-                                    <KeyboardReturn />
-                                </IconButton>
+                                                // Clear message box
+                                                this.setState({
+                                                    ...this.state,
+                                                    message: ''
+                                                });
+                                            }
+                                        }}
+                                    />
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
-                    </Grid>
                 </Paper>
 
-                {/* Right Drawer */ }
-        <Drawer variant='permanent' anchor='right' className={classes.listRight}
-            classes={{
-                paper: classes.paperRight
-            }}>
-            <Toolbar />
-            <List>
-                <ListItem className={classes.listHeader}>
-                    <ListItemText className={classes.listHeaderText}>
-                        ACTION MENU
-                            </ListItemText>
-                </ListItem>
-                <Divider />
-                <ListItem className={classes.actionItem}>
-                    <UserSearchDialog />
-                    <Button fullWidth color='primary' onClick={() => {
-                        showUserSearchDialog();
-                    }}>Search User</Button>
-                </ListItem>
-                <ListItem className={classes.actionItem}>
-                    <Button fullWidth color='primary' onClick={() => {
-
-                    }}>Refresh Key</Button>
-                </ListItem>
-                <ListItem className={classes.actionItem}>
-                    <FriendRequestDialog />
-                    <Button fullWidth color='primary' onClick={() => {
-                        showFriendRequestDialog();
+                {/* Right Drawer */}
+                <Drawer variant='permanent' anchor='right' className={classes.listRight}
+                    classes={{
+                        paper: classes.paperRight
                     }}>
-                        {
-                            friendRequests.length && (
-                                <Badge badgeContent={friendRequests.length} color='secondary'>
-                                    Friend Requests
-                                    </Badge>
-                            ) || 'Friend Requests'
-                        }
+                    <Toolbar />
+                    <List>
+                        <ListItem className={classes.listHeader}>
+                            <ListItemText className={classes.listHeaderText}>
+                                ACTION MENU
+                                    </ListItemText>
+                        </ListItem>
+                        <Divider />
+                        <ListItem className={classes.actionItem}>
+                            <UserSearchDialog />
+                            <Button fullWidth color='primary' onClick={() => {
+                                showUserSearchDialog();
+                            }}>Search User</Button>
+                        </ListItem>
+                        <ListItem className={classes.actionItem}>
+                            <Button fullWidth color='primary' onClick={() => {
 
-                    </Button>
-                </ListItem>
-            </List>
-        </Drawer>
+                            }}>Refresh Key</Button>
+                        </ListItem>
+                        <ListItem className={classes.actionItem}>
+                            <FriendRequestDialog />
+                            <Button fullWidth color='primary' onClick={() => {
+                                showFriendRequestDialog();
+                            }}>
+                                {
+                                    friendRequests.length && (
+                                        <Badge badgeContent={friendRequests.length} color='secondary'>
+                                            Friend Requests
+                                        </Badge>
+                                    ) || 'Friend Requests'
+                                }
+
+                            </Button>
+                        </ListItem>
+                    </List>
+                </Drawer>
             </React.Fragment >
         );
     }
